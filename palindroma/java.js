39a3b6656840a2ae1191cd6cb16901638900e340
Francesca Palumbo <econofra87@gmail.com>
2020-04-29 17:01:50 +0200

    da finire

--- palindroma/java.js
+++ palindroma/java.js
@@ -1,18 +1,26 @@
 // scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
 
-// chiedo una parola all'utente
+
+// chiedo all'utente una parola
+var parola_richiesta = prompt ( "inserisca una parola: ");
+
+// voglio isolare ogni lettera della parola inserita dall'utente
+var singole_lettere = parola_richiesta.split("");
+
+// creo un array dove inserirò le lettere messe al contrario
+var lettere_inverse =[];
+// una volta che ho isolato le singole lettere della parola inseirta dall'utente creo un ciclo for che parte dall'ultima lettera dell 
+for (var i = singole_lettere.length-1; i >= 0; i--) {
+    console.log(singole_lettere[i]);
+    lettere_inverse.push(singole_lettere[i]);
+}
 
 
-// function is_palindromo ( parola_richiesta){
-    var parola_richiesta = prompt ( "inserisca una parola: ");
-    var singole_lettere = parola_richiesta.split("");
-        // creo un array con la parola inserita dall'utente
-        console.log(singole_lettere);
+var parola_inversa = lettere_inverse.join("");
+console.log(parola_inversa);
 
-        for (var i = 0; i=0 = i++; i++) {
-        }if (singole_lettere.charAt(0)=singole_lettere.charAt(singole_lettere.length-1)) {
-                console.log("la parola è palindroma");
-            } else{
-                console.log("la parola non è palindroma");
-        }
-    // }
+if (parola_inversa==parola_richiesta) {
+    console.log("la parola inserita dall'utente è palindroma");
+} else {
+    console.log("la parola inserita dall'utente non è palindroma");
+}
